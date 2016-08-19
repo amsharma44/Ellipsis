6a6ea15e385fcf064d576748f1417ee23af48d36
Aman Sharma <amsharma44@outlook.com>
2016-08-19 23:20:51 +0530

    Begin the project development

---
+++ Ellipsis/Services/ConverterService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ellipsis.Services
+{
+    /// <summary>
+    /// Helper class to convert video streams using NReco.VideoConvert
+    /// </summary>
+    public class ConverterService
+    {
+        // TODO: Implement Service
+    }
+}
---
+++ Ellipsis/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ellipsis.ViewModels
+{
+    public class MainPageViewModel
+    {
+        // TODO: Implement view model for "mainpage"
+    }
+}
